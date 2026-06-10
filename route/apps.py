d4cc0c4c2f6d5a14d5df7b7624df27a172cf9d07
Kim-AII <karimdutrez07@gmail.com>
2026-06-10 07:37:16 +0100

    supression et unification des fichiers css et js et Html et début de la phase de tests

--- route/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MentorlinkApiConfig(AppConfig):
-    name = 'mentorlink_api'
--- route/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/inscription/', views.api_inscription, name='api_inscription'),
-    path('api/connexion/', views.api_connexion, name='api_connexion'),
-    path('api/utilisateurs/', views.api_utilisateurs, name='api_utilisateurs'),
-]
