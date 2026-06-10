b3e0010049c8755b95d4cbeb2f47edfff5c102c9
Kim-AII <karimdutrez07@gmail.com>
2026-06-10 03:40:26 +0100

    remodelage de l'agencement des fichiers et suppression de certains fichiers inutiles

---
+++ run.py
@@ -0,0 +1,10 @@
+from PIL1_2526_68.app import create_app
+from extensions import db
+
+app = create_app()
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()   # Crée les tables si elles n'existent pas
+        print("✅ Tables créées.")
+    app.run(debug=True, host='0.0.0.0', port=5000)
